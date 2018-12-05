b17fa085e4c306587788787d3bbbcc2faf3c0f1a
D.Biles <darron.biles@gmail.com>
2018-12-05 02:24:18 -0500

    described test for htmlroutes

---
+++ test/routes/htmlRoutes.test.js
@@ -0,0 +1,13 @@
+const chai = require("chai").expect;
+const chaihttp = require("chai-http");
+var should = chai.should();
+
+chai.use(chaihttp);
+
+describe("HTML Routes", function() {
+  it("should /GET the home page");
+  it("should /GET the about page");
+  it("should open the registration page");
+  it("should open the login page");
+  it("should catch all routes not defined the login page");
+});
