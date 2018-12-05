177c43422e8772199218d287c3605aa38edce31e
Peter Huynh <huynhxpeter@gmail.com>
2018-12-05 03:19:35 -0500

    modal for tenant maint request

--- public/js/tenant.js
+++ public/js/tenant.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
-  $(".dropdown-trigger").dropdown();
-  $(".modal").modal('onOpenStart');
-  $("select").formSelect();
+$(function() {
+  $("#submitBtn").on("click", function(event) {
+    event.preventDefault();
+
+    // maintenance request object
+    const maintRequest = {
+      requestType: $("#requestType").val(),
+      description: $("#textarea1").val()
+    };
+
+    // $.ajax("/tenant/maintenance", {
+    //   type: "POST",
+    //   data: maintRequest
+    // }).then(function() {
+    //   console.log("new maintenance request submitted successfully");
+    //   location.reload();
+    // });
+
+    console.log(maintRequest);
+  });
 });
+
+// take maintenance requests and place them in cards
