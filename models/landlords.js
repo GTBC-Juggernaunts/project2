687c0969481910564e3a0d0093e6b1a5f9367939
Mike Gross <grossmi4@gmail.com>
2018-12-05 18:29:50 -0500

    reverted users table to landlords

---
+++ models/landlords.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+  const landlord = sequelize.define("landlord", {
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  landlord.associate = function(models) {
+    landlord.belongsTo(models.tenant, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  landlord.associate = function(models) {
+    landlord.hasMany(models.property, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  landlord.associate = function(models) {
+    landlord.hasMany(models.maintenance, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  return landlord;
+};
