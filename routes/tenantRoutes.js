ed47d55e0610a42f5e3d6db399a5ac4e839dc196
Peter Huynh <huynhxpeter@gmail.com>
2018-12-06 19:31:29 -0500

    added tenantRoutes.test.js

--- routes/tenantRoutes.js
+++ routes/tenantRoutes.js
@@ -1,14 +1,20 @@
 // Dependencies
 // *************************************************************
 // model
-const db = require("../models");
+// const db = require("../models");
 
 // Routes
 // *************************************************************
 module.exports = app => {
-  // Load the maintenance page when a tenant logs in
+  // GET request to load maintenance page when a tenant logs in
   app.get("/tenant/maintenance", (req, res) => {
     res.render("tenant-maint", { maintRequest: "Hello World" });
+  });
+
+  // POST request to create new maintenance request
+  app.post("/tenant/maintenance", (req, res) => {
+    console.log(req.body);
+    console.log(res);
   });
 
   // Load the payments page when a tenant logs in
