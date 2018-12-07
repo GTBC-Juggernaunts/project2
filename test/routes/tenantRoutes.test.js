335b88c427e1a7871c45a9ca9fa0de5c7d9d6afa
Peter Huynh <huynhxpeter@gmail.com>
2018-12-06 19:42:48 -0500

    fixed tenantroutes test

--- test/routes/tenantRoutes.test.js
+++ test/routes/tenantRoutes.test.js
@@ -12,36 +12,50 @@ var request;
 describe("GET /tenant/maintenance", function() {
   // Before each test begins, create a new request server for testing
   // & delete all examples from the db
-  beforeEach(function() {
+  beforeEach(function(done) {
     request = chai.request(server);
-    return db.sequelize.sync({ force: true });
+    db.sequelize.sync({ force: true }).then(function() {
+      done();
+    });
   });
 
   it("should find all examples", function(done) {
     // Add some examples to the db to test with
-    db.maintenancerequest.bulkCreate([{ description: "First Description" }]).then(function() {
-      // Request the route that returns all examples
-      request.get("/teanant/maintenance").end(function(err, res) {
-        var responseStatus = res.status;
-        var responseBody = res.body;
+    db.maintenancerequest
+      .bulkCreate([
+        { description: "First Description" },
+        { description: "Second Description" },
+        { description: "Third Description" }
+      ])
+      .then(function() {
+        // Request the route that returns all examples
+        request.get("/teanant/maintenance").end(function(err, res) {
+          var responseStatus = res.status;
+          var responseBody = res.body;
 
-        // Run assertions on the response
+          // Run assertions on the response
 
-        expect(err).to.be.null;
+          expect(err).to.be.null;
 
-        expect(responseStatus).to.equal(200);
+          expect(responseStatus).to.equal(200);
 
-        expect(responseBody)
-          .to.be.an("array")
-          .that.has.lengthOf(1);
+          expect(responseBody)
+            .to.be.an("array")
+            .that.has.lengthOf(3);
 
-        expect(responseBody[0])
-          .to.be.an("object")
-          .that.includes({ description: "First Description" });
+          expect(responseBody[0])
+            .to.be.an("object")
+            .that.includes({ description: "First Description" });
+          expect(responseBody[1])
+            .to.be.an("object")
+            .that.includes({ description: "Second Description" });
+          expect(responseBody[2])
+            .to.be.an("object")
+            .that.includes({ description: "Third Description" });
 
-        // The `done` function is used to end any asynchronous tests
-        done();
+          // The `done` function is used to end any asynchronous tests
+          done();
+        });
       });
-    });
   });
 });
