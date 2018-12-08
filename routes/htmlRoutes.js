b04268279bb0c051c6d3ee29d75c8c3f9ac11bc2
D.Biles <darron.biles@gmail.com>
2018-12-08 01:43:59 -0600

    Updated routes and html pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,16 @@
 const db = require("../models");
-
+const path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.status(200);
+    res.sendFile(path.join(__dirname, "../public/html/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load about page
+  app.get("/about-us", (req, res) => {
+    res.status(200);
+    res.sendFile(path.join(__dirname, "../public/html/aboutus.html"));
   });
 
   // Render 404 page for any unmatched routes
