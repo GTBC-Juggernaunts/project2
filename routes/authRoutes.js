d16d3a571985a88dd95da1ded4e015a671dca998
Mike <grossmi4@gmail.com>
2018-12-08 13:17:19 -0500

    commented code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,37 +4,41 @@ var passport = require("passport");
 // Routes
 // *************************************************************
 module.exports = app => {
+  // Tenant login
   app.post("/login/tenant", function(req, res, next) {
     passport.authenticate("local", function(err, user) {
       if (err) {
-        return next(err);
+        return next(err); // Not sure what this does
       }
       if (!user) {
-        return res.redirect("/").status(401);
+        return res.redirect("/").status(401); // If login/password don't match
       }
+      // Logs user in for session
       req.logIn(user, function(err) {
         if (err) {
           return next(err);
         }
+        // Directs user to tenant maintenance page on successful login
         return res.redirect("/tenant/maintenance");
       });
     })(req, res, next);
   });
-};
 
-
+  // Landlord login
   app.post("/login/landlord", function(req, res, next) {
     passport.authenticate("local", function(err, user) {
       if (err) {
         return next(err);
       }
       if (!user) {
-        return res.redirect("/").status(401);
+        return res.redirect("/").status(401); //on failed login redirects to root page
       }
+      //Logs user in for session
       req.logIn(user, function(err) {
         if (err) {
           return next(err);
         }
+        // Directs user to properties page
         return res.redirect("/landlord/properties");
       });
     })(req, res, next);
